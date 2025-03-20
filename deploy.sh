d90a39ac7febaac886bd4bf72349d82dd811a804
Shafeer Ahamed M <126599497+shafeer2511@users.noreply.github.com>
2025-03-20 12:09:21 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u shafeer2511 -p dckr_pat_k_8kc1UGKRNeVGS7n2mxvzxpONk
-    docker tag test shafeer2511/dev
-    docker push shafeer2511/dev
+    docker tag test1 shafeer2511/test
+    docker push shafeer2511/test
+    
