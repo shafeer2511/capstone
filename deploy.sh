48df345b707bf8f7d6d194c0ddf17a174aa76772
Shafeer Ahamed M <126599497+shafeer2511@users.noreply.github.com>
2025-03-21 13:53:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@
     docker login -u shafeer2511 -p dckr_pat_k_8kc1UGKRNeVGS7n2mxvzxpONk
     docker tag test1 shafeer2511/test
     docker push shafeer2511/test
-    docker compose hub -d    
+    docker compose up -d    
